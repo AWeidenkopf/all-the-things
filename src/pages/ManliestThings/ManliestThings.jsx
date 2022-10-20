d5cbf3fb3c993ddf5e0bbf9cb0cbeeee832904ca
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-10-20 13:27:08 -0400

    change manliestThings h1 to Anchis things

--- src/pages/ManliestThings/ManliestThings.jsx
+++ src/pages/ManliestThings/ManliestThings.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const ManliestThings = (props) => {
   return (
     <>
-      <h1>Ben's Things</h1>
+      <h1>ANCHI's Things</h1>
       <Link to="/">Home</Link>
 
       {props.things.map((thing, idx) => 
